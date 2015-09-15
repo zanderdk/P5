de7f08990e8939f26af3f21888cfc69d0ae25735
Mathias <mpbk13@student.aau.dk>
2015-09-15 09:16:29 +0200

    ljbkvgiu

--- main.c
+++ main.c
@@ -9,21 +9,31 @@
 
     void ecrobot_device_initialize(void)
     {
+        ecrobot_init_dist_sensor(NXT_PORT_S1, RANGE_MEDIUM);
+        ecrobot_init_dist_sensor(NXT_PORT_S2, RANGE_MEDIUM);
     }
     
     void ecrobot_device_terminate(void)
     {
+        ecrobot_term_dist_sensor(NXT_PORT_S1);
+        ecrobot_term_dist_sensor(NXT_PORT_S2);
     }
 
 
     TASK(OSEK_Task_Background)
     {
-        while(1)
-        {
-            ecrobot_set_motor_mode_speed(NXT_PORT_A, 0, 100);
-            ecrobot_set_motor_mode_speed(NXT_PORT_B, 0, 100);
-            display_goto_xy(0,0);
-            display_update();
-            systick_wait_ms(50);
-        }
+      while(1){
+    	  display_goto_xy(0, 0);
+    	  display_string("Distance 1:");
+    	  display_goto_xy(0, 1);
+        U32 dis = ecrobot_get_dist_sensor(NXT_PORT_S1);
+        display_goto_xy(0,0);
+    	  display_string("Distance 2:");
+    	  display_goto_xy(0,4);
+    	  display_int(ecrobot_get_dist_sensor(NXT_PORT_S2), 5);
+    	  ecrobot_set_motor_mode_speed(NXT_PORT_A, 0, 5);
+    	  display_update();
+
+    	  systick_wait_ms(50);
+      }
     }
