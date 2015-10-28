0906aa4a24f7ff033acfe7dea6169961605eda7c
Mathias <mpbk13@student.aau.dk>
2015-10-28 15:17:29 +0100

    Same as before

---
+++ olddist.h
@@ -0,0 +1,15 @@
+#include "ecrobot_types.h"
+
+#define RANGE_NORMAL     0
+#define RANGE_SHORT      1
+#define RANGE_MEDIUM     2
+#define RANGE_LONG       4
+
+/* NXT medium range distance sensor API */
+extern void ecrobot_init_dist_sensor(U8 port_id, U8 range);
+extern  S32 ecrobot_get_dist_sensor(U8 port_id);
+extern void ecrobot_term_dist_sensor(U8 port_id);
+extern U16 ecrobot_get_obstical_detection_sensor(U8 port_id);
+extern void ecrobot_init_obstical_detection_sensor(U8 port_id, U8 range);
+
+
