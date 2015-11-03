216d08dd8a8bf5b447600b1e1a920caed927fe06
Lasse Just <lasse_just@outlook.com>
2015-11-03 13:49:16 +0100

    Updated position_check previous measurement stuff

--- tracking.c
+++ tracking.c
@@ -13,45 +13,57 @@
 #define RANGE_FAR 300
 #define RANGE_CLOSE 700
 
-S8 position_check(U8 left_sensor, U8 right_sensor, S8 prev){
+#define MOTOR_FAST 80
+#define MOTOR_MED  75
+#define MOTOR_SLOW 70
+#define MOTOR_STOP  0
+
+S8 position_check(U8 left_sensor, U8 right_sensor){
+	static S8 prev = 0;
 	S32 left = (S32)ecrobot_get_dist_sensor(left_sensor);
 	S32 right = (S32)ecrobot_get_dist_sensor(right_sensor);
 
 	if(left < RANGE_CLOSE && right < RANGE_CLOSE)
-		return TARGET_CENTER;
+		return prev = TARGET_CENTER;
 	if(left < RANGE_FAR)
-		return TARGET_MED_LEFT;
-	if(left < RANGE_CLOSE)
+		return prev = TARGET_MED_LEFT;
+	if(left < RANGE_CLOSE){
 		if(prev == TARGET_UNKNOWN)
-			return TARGET_FAR_LEFT;
-		return TARGET_LEFT;
+			return prev = TARGET_FAR_LEFT;
+		return prev = TARGET_LEFT;
+	}
 	if(right < RANGE_FAR)
-		return TARGET_MED_RIGHT;
-	if(right < RANGE_CLOSE)
+		return prev = TARGET_MED_RIGHT;
+	if(right < RANGE_CLOSE){
 		if(prev == TARGET_UNKNOWN)
-			return TARGET_FAR_RIGHT;
-		return TARGET_RIGHT;
-	else return TARGET_UNKNOWN;
+			return prev = TARGET_FAR_RIGHT;
+		return prev = TARGET_RIGHT;
+	}
+	if(prev < 0)
+		return prev = TARGET_FAR_LEFT;
+	if(prev > 0)
+		return prev = TARGET_FAR_RIGHT;
+	else return prev =TARGET_UNKNOWN;
 }
 
 S8 naive_speed(S8 reading){
 	switch(reading){
-		case -3:
-			return -100
-		case -2:
-			return -80;
-		case -1:
-			return -60;
-		case 0:
-			return 0;
-		case 1:
-			return 60;
-		case 2:
-			return 80;
-		case 3:
-			return 100;
+		case TARGET_FAR_LEFT:
+			return -MOTOR_FAST;
+		case TARGET_MED_LEFT:
+			return -MOTOR_MED;
+		case TARGET_LEFT:
+			return -MOTOR_SLOW;
+		case TARGET_CENTER:
+			return MOTOR_STOP;
+		case TARGET_RIGHT:
+			return MOTOR_SLOW;
+		case TARGET_MED_RIGHT:
+			return MOTOR_MED;
+		case TARGET_FAR_RIGHT:
+			return MOTOR_FAST;
 		default:
-			return 0;
+			return MOTOR_STOP;
 	}
 	
 	/*
