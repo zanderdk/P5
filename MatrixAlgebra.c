a653a3386b00a37c59136995a147203cf6135802
Kenneth <super-stick@hotmail.com>
2015-11-06 10:32:26 +0100

    matrix algebra

--- MatrixAlgebra.c
+++ MatrixAlgebra.c
@@ -7,35 +7,40 @@
 
 #include "ecrobot_types.h"
 
-void MatrixAddition(int row, int columns, double firstMatrix[columns][row], double secondMatrix[columns][row], double rtnMatrix[columns][row])
+void MatrixAddition(int columns, int row, double firstMatrix[columns][row], double secondMatrix[columns][row], double rtnMatrix[columns][row])
 {
+	double firstMatrixTmp[columns][row] = firstMatrix;
+	double secondMatrixTmp[columns][row] = secondMatrix;
 	int i;
 	int j;
 	for(i=0; i<columns; i++)
 	{
 		for(j=0; j<row; j++)
 		{
-			rtnMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+			rtnMatrix[i][j] = firstMatrixTmp[i][j] + secondMatrixTmp[i][j];
 		}
 	}
 }
 
-void SkalarMultiplikation(int row, int columns, double matrix[columns][row], int multiplier, double rtnMatrix[columns][row])
+void SkalarMultiplikation(int columns, int row, double matrix[columns][row], int multiplier, double rtnMatrix[columns][row])
 {
+		double matrixTmp[columns][row] = matrix;
 		int i = 0;
 		int j = 0;
 		for(i=0; i<columns; i++)
 		{
 			for(j=0; j<row; j++)
 			{
-				rtnMatrix[i][j] = matrix[i][j] * multiplier;
+				rtnMatrix[i][j] = matrixTmp[i][j] * multiplier;
 			}
 		}
 
 }
 
-S8 MatrixMultiplikation(int rowFirst, int columnsFirst, int rowSecond, int columnsSecond, double firstMatrix[columnsFirst][rowFirst], double secondMatrix[columnsSecond][rowSecond], double rtnMatrix[columnsFirst][rowSecond])
+S8 MatrixMultiplikation(int columnsFirst, int rowFirst, int columnsSecond, int rowSecond, double firstMatrix[columnsFirst][rowFirst], double secondMatrix[columnsSecond][rowSecond], double rtnMatrix[columnsFirst][rowSecond])
 {
+	double firstMatrixTmp[columnsFirst][rowFirst] = firstMatrix;
+	double  double secondMatrixTmp[columnsSecond][rowSecond] = secondMatrix;
 	double sum = 0;
 	int i;
 	int j;
@@ -48,7 +53,7 @@ S8 MatrixMultiplikation(int rowFirst, int columnsFirst, int rowSecond, int colum
 			{
 				for(k=0; k<columnsSecond; k++)
 				{
-					sum += firstMatrix[i][k]*secondMatrix[k][j];
+					sum += firstMatrixTmp[i][k]*secondMatrixTmp[k][j];
 				}
 			}
 		}
@@ -61,15 +66,16 @@ S8 MatrixMultiplikation(int rowFirst, int columnsFirst, int rowSecond, int colum
 
 }
 
-void MatrixTranspose(int row, int columns, double Matrix[columns][row], double rtnMatrix[columns][row])
+void MatrixTranspose(int columns, int row, double Matrix[columns][row], double rtnMatrix[columns][row])
 {
+	double MatrixTmp[columns][row] = Matrix;
 	int i = 0;
 	int j = 0;
 	for(i=0; i<columns; i++)
 	{
 		for(j=0; j<row; j++)
 		{
-			rtnMatrix[j][i] = Matrix[i][j];
+			rtnMatrix[j][i] = MatrixTmp[i][j];
 		}
 	}
 }
@@ -77,12 +83,13 @@ void MatrixTranspose(int row, int columns, double Matrix[columns][row], double r
 
 S8 MatrixInvers(int row, int columns, double Matrix[columns][row], double rtnMatrix[columns][row])
 {
+	double MatrixTmp[columns][row] = Matrix;
 	if(row == 2 && columns == 2)
 	{
-		rtnMatrix[0][0] = Matrix[1][1];
-		rtnMatrix[0][1] = -Matrix[1][0];
-		rtnMatrix[1][0] = -Matrix[0][1];
-		rtnMatrix[1][1] = Matrix[0][0];
+		rtnMatrix[0][0] = MatrixTmp[1][1];
+		rtnMatrix[0][1] = -MatrixTmp[1][0];
+		rtnMatrix[1][0] = -MatrixTmp[0][1];
+		rtnMatrix[1][1] = MatrixTmp[0][0];
 		return 1;
 	}
 	else
